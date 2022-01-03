a16872aa979266f46f180b028689cdd5c52d7095
seungjun123 <ssj0187@naver.com>
2022-01-03 22:02:23 +0900

    1/3

---
+++ 문자열 반복.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n):
+    text = ""
+    num, temp = input().split()
+    num = int(num)
+    for j in range(len(temp)):
+        text += temp[j]*num
+    print(text)
